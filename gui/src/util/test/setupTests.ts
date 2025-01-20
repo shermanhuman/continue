7586c5b2b5a0c264864e9d57be23bb59b44921de
Sherman Boyd <sherman.boyd@gmail.com>
2025-01-20 13:13:30 -0700

    attempt to resolve env not configured to support act errors

--- gui/src/util/test/setupTests.ts
+++ gui/src/util/test/setupTests.ts
@@ -1,4 +1,7 @@
-import "@testing-library/jest-dom";
+import { expect, afterEach, afterAll, vi } from 'vitest';
+import * as jestDomMatchers from '@testing-library/jest-dom/matchers';
+
+expect.extend(jestDomMatchers);
 
 afterEach(() => {
   vi.clearAllMocks();
@@ -22,3 +25,4 @@ Object.defineProperty(window, "matchMedia", {
     dispatchEvent: vi.fn(),
   })),
 });
+
